1b3c74ce8de1c851ad749421dd8420dc8e27cba5
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-12-26 04:18:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onscroll = () =>{
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active');
+        
+    } else {
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
